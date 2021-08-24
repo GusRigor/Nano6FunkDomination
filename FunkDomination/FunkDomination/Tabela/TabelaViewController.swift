a5e68ddc7aacf7b19e28d6ab0d2b132650855636
Gustavo Rigor <gusrigor@icloud.com>
2021-08-24 16:07:03 -0300

    Add: table ao view controller com os upgrades

--- FunkDomination/FunkDomination/Tabela/TabelaViewController.swift
+++ FunkDomination/FunkDomination/Tabela/TabelaViewController.swift
@@ -7,25 +7,29 @@
 
 import UIKit
 
-class TabelaViewController: UIViewController {
+class TabelaViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     @IBOutlet weak var upgradeTableView: UITableView!
     @IBOutlet weak var tituloTabelLabel: UILabel!
     
+    let listaUpgrades = UpgradeDAO().retornaTodosOsUpgrades()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        upgradeTableView.dataSource = self
+        upgradeTableView.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        listaUpgrades.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let upgrade = listaUpgrades[indexPath.row]
+        cell.textLabel?.text = upgrade.titulo
+        
+        return cell
+    }
+    
 }
