3ced24aa9013a23be5dd8772ff7058b129e585f4
Gustavo Rigor <gusrigor@icloud.com>
2021-08-26 11:21:44 -0300

    Ajuste na identação

--- FunkDomination/FunkDomination/GameScene.swift
+++ FunkDomination/FunkDomination/GameScene.swift
@@ -9,73 +9,73 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-     
-     var menuOpen = false
     
-     var numDinheiro = 0.0
-     var numSeguidores = 0.0
-     var coefGanhoDinheiro = 1.0
-     var coefGanhoSeguidores = 1.0
-     var timer = TimeInterval.init(10000000)
-
-     
-     let imageButton = SKSpriteNode(color: .blue, size: .init(width: 100, height: 100))
-     let labelButton = SKLabelNode(text: "+")
-     let labelDinheiro = SKLabelNode(text: "0")
-     let labelSeguidores = SKLabelNode(text: "0")
-     
-     let imageMenuButton = SKSpriteNode(color: .blue, size: .init(width: 100, height: 100))
-     let labelMenuButton = SKLabelNode(text: "Menu")
-     
-     let imageMenuCloseButton = SKSpriteNode(color: .black, size: .init(width: 100, height: 100))
-     let labelMenuCloseButton = SKLabelNode(text: "X")
-     
-     
-     let menu = SKSpriteNode(color: .green, size: .init(width: 650, height: 600))
-     
-     
-     override func sceneDidLoad() {
-          super.sceneDidLoad()
-          menu.position = CGPoint(x: 0, y: 0)
-          menu.isHidden = true
+    var menuOpen = false
+    
+    var numDinheiro = 0.0
+    var numSeguidores = 0.0
+    var coefGanhoDinheiro = 1.0
+    var coefGanhoSeguidores = 1.0
+    var timer = TimeInterval.init(10000000)
+    
+    
+    let imageButton = SKSpriteNode(color: .blue, size: .init(width: 100, height: 100))
+    let labelButton = SKLabelNode(text: "+")
+    let labelDinheiro = SKLabelNode(text: "0")
+    let labelSeguidores = SKLabelNode(text: "0")
+    
+    let imageMenuButton = SKSpriteNode(color: .blue, size: .init(width: 100, height: 100))
+    let labelMenuButton = SKLabelNode(text: "Menu")
+    
+    let imageMenuCloseButton = SKSpriteNode(color: .black, size: .init(width: 100, height: 100))
+    let labelMenuCloseButton = SKLabelNode(text: "X")
+    
+    
+    let menu = SKSpriteNode(color: .green, size: .init(width: 650, height: 600))
+    
+    
+    override func sceneDidLoad() {
+        super.sceneDidLoad()
+        menu.position = CGPoint(x: 0, y: 0)
+        menu.isHidden = true
         
-          labelButton.fontSize = 45
-          labelButton.verticalAlignmentMode = .center
-          labelButton.fontColor = .white
+        labelButton.fontSize = 45
+        labelButton.verticalAlignmentMode = .center
+        labelButton.fontColor = .white
         
-          labelDinheiro.fontSize = 50
-          labelDinheiro.verticalAlignmentMode = .center
-          labelDinheiro.fontColor = .white
-          
-          labelSeguidores.fontSize = 50
-          labelSeguidores.verticalAlignmentMode = .center
-          labelSeguidores.fontColor = .white
-          
-          labelMenuButton.fontSize = 40
-          labelMenuButton.verticalAlignmentMode = .center
-          labelMenuButton.fontColor = .white
-          
-          labelMenuCloseButton.fontSize = 50
-          labelMenuCloseButton.verticalAlignmentMode = .center
-          labelMenuCloseButton.fontColor = .white
+        labelDinheiro.fontSize = 50
+        labelDinheiro.verticalAlignmentMode = .center
+        labelDinheiro.fontColor = .white
         
-          let button = SKButtonNode(image: imageButton, label: labelButton, action: {
-               self.numSeguidores += 1 * self.coefGanhoSeguidores
-               self.labelSeguidores.text = "\(self.numSeguidores)"
-          })
-          
-          let buttonClose = SKButtonNode(image: self.imageMenuCloseButton, label: self.labelMenuCloseButton, action: {
-               self.menu.isHidden = true
-               self.menuOpen = false
-          })
-          
-          let buttonMenu = SKButtonNode(image: imageMenuButton, label: labelMenuButton, action: {
-               if !self.menuOpen{
-                    buttonClose.position = CGPoint(x: 0, y: -250)
-                    self.menu.isHidden = false
-                    self.menuOpen = true
-               }
-          })
+        labelSeguidores.fontSize = 50
+        labelSeguidores.verticalAlignmentMode = .center
+        labelSeguidores.fontColor = .white
+        
+        labelMenuButton.fontSize = 40
+        labelMenuButton.verticalAlignmentMode = .center
+        labelMenuButton.fontColor = .white
+        
+        labelMenuCloseButton.fontSize = 50
+        labelMenuCloseButton.verticalAlignmentMode = .center
+        labelMenuCloseButton.fontColor = .white
+        
+        let button = SKButtonNode(image: imageButton, label: labelButton, action: {
+            self.numSeguidores += 1 * self.coefGanhoSeguidores
+            self.labelSeguidores.text = "\(self.numSeguidores)"
+        })
+        
+        let buttonClose = SKButtonNode(image: self.imageMenuCloseButton, label: self.labelMenuCloseButton, action: {
+            self.menu.isHidden = true
+            self.menuOpen = false
+        })
+        
+        let buttonMenu = SKButtonNode(image: imageMenuButton, label: labelMenuButton, action: {
+            if !self.menuOpen{
+                buttonClose.position = CGPoint(x: 0, y: -250)
+                self.menu.isHidden = false
+                self.menuOpen = true
+            }
+        })
         
         let menuLabel = SKLabelMenuNode("Publicação nas redes sociais R$ 10", position: .init(x: 0, y: 300), action: {
             if self.numDinheiro > 10{
@@ -85,27 +85,27 @@ class GameScene: SKScene {
             }
         })
         
-          button.position = CGPoint(x: 0, y: -500)
-          buttonMenu.position = CGPoint(x: -200, y: -500)
-          labelDinheiro.position = CGPoint(x: 300, y: 500)
-          labelSeguidores.position = CGPoint(x: -300, y: 500)
-          self.addChild(menu)
-          menu.addChild(buttonClose)
+        button.position = CGPoint(x: 0, y: -500)
+        buttonMenu.position = CGPoint(x: -200, y: -500)
+        labelDinheiro.position = CGPoint(x: 300, y: 500)
+        labelSeguidores.position = CGPoint(x: -300, y: 500)
+        self.addChild(menu)
+        menu.addChild(buttonClose)
         menu.addChild(menuLabel)
-          self.addChild(button)
-          self.addChild(buttonMenu)
-          self.addChild(labelDinheiro)
-          self.addChild(labelSeguidores)
+        self.addChild(button)
+        self.addChild(buttonMenu)
+        self.addChild(labelDinheiro)
+        self.addChild(labelSeguidores)
     }
-     
-     override func update(_ currentTime: TimeInterval) {
-          if timer > currentTime  {
-               timer = currentTime
-          }
-          if (currentTime - timer) >= 1{
-               self.numDinheiro += self.numSeguidores * self.coefGanhoDinheiro
-               self.labelDinheiro.text = "\(numDinheiro)"
-               timer = currentTime
-          }
-     }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if timer > currentTime  {
+            timer = currentTime
+        }
+        if (currentTime - timer) >= 1{
+            self.numDinheiro += self.numSeguidores * self.coefGanhoDinheiro
+            self.labelDinheiro.text = "\(numDinheiro)"
+            timer = currentTime
+        }
+    }
 }
